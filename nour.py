04210bc2f3d5f9cb31f47a5b47eb1bc1b6cd1970
abd3434 <145936250+abd3434@users.noreply.github.com>
2023-09-25 00:39:15 +0300

    Update nour.py

--- nour.py
+++ nour.py
@@ -10,8 +10,23 @@ st.title("Healthcare Data Visualization")
 
 # Filter the data based on conditions
 st.sidebar.header("Filter Data")
+
+# Age Range Slider
 age_range = st.sidebar.slider("Select Age Range", int(df['age'].min()), int(df['age'].max()), (int(df['age'].min()), int(df['age'].max())))
-filtered_df = df[(df['age'] >= age_range[0]) & (df['age'] <= age_range[1])]
+
+# Hypertension Checkbox
+hypertension_checkbox = st.sidebar.checkbox("Hypertension", True)
+
+# Heart Disease Checkbox
+heart_disease_checkbox = st.sidebar.checkbox("Heart Disease", True)
+
+filtered_df = df[(df['age'] >= age_range[0]) & (df['age'] <= age_range[1]) & 
+                  (df['hypertension'] == int(hypertension_checkbox)) & 
+                  (df['heart_disease'] == int(heart_disease_checkbox))]
+
+# Display the filtered data
+st.write("Filtered Data:")
+st.write(filtered_df)
 
 # 3D Scatter Plot
 st.subheader("3D Scatter Plot of Age, Glucose Level, and BMI")
@@ -29,30 +44,30 @@ fig_contour = px.density_contour(filtered_df, x='age', y='avg_glucose_level', co
                                  color_discrete_map={0: 'blue', 1: 'red'})
 st.plotly_chart(fig_contour)
 
-# Scatterplot Matrix
-st.subheader("Scatterplot Matrix")
-numerical_columns = ['age', 'avg_glucose_level', 'bmi']
-fig_scatter_matrix = px.scatter_matrix(filtered_df, dimensions=numerical_columns, color='hypertension',
-                                       title='Scatterplot Matrix Hypertension for Hypertension',
-                                       labels={col: col for col in numerical_columns})
-st.plotly_chart(fig_scatter_matrix)
+# Dropdown for Work Type
+work_type_options = ['Private', 'Self-employed', 'Govt_job', 'children', 'Never_worked']
+selected_work_type = st.sidebar.selectbox("Select Work Type", work_type_options)
+work_type_filtered_df = filtered_df[filtered_df['work_type'] == selected_work_type]
 
-# Sunburst Chart
-st.subheader("Sunburst Chart: Relationship between Smoking Status, Gender, and Stroke")
-fig_sunburst = px.sunburst(filtered_df, path=['smoking_status', 'gender'], values='stroke',
-                           title='Sunburst Chart: Relationship between Smoking Status, Gender, and Stroke',
-                           color_continuous_scale='Viridis',
-                           labels={'smoking_status': 'Smoking Status', 'gender': 'Gender', 'stroke': 'Stroke'})
-st.plotly_chart(fig_sunburst)
+# Pie Chart
+st.subheader(f"Pie Chart: Distribution of Stroke for {selected_work_type} Work Type")
+fig_pie = px.pie(work_type_filtered_df, names='stroke', title=f"Distribution of Stroke for {selected_work_type} Work Type")
+st.plotly_chart(fig_pie)
 
-# Animated Bar Chart
-st.subheader("Animated Bar Chart: Strokes Over Age")
-fig_bar = px.bar(filtered_df, x='age', y='stroke', title='Animated Bar Chart: Strokes Over Age',
-                  labels={'age': 'Age', 'stroke': 'Stroke'},
-                  animation_frame='age',
-                  range_x=[int(df['age'].min()), int(df['age'].max())],
-                  range_y=[0, 1],
-                  color='stroke',
-                  color_discrete_map={0: 'blue', 1: 'red'},
-                  text='stroke')
-st.plotly_chart(fig_bar)
+# Bar Chart with Range Slider for Glucose Level
+st.subheader("Bar Chart: Strokes Over Glucose Level")
+glucose_level_range = st.sidebar.slider("Select Glucose Level Range", float(df['avg_glucose_level'].min()), 
+                                       float(df['avg_glucose_level'].max()), 
+                                       (float(df['avg_glucose_level'].min()), float(df['avg_glucose_level'].max())))
+glucose_filtered_df = df[(df['avg_glucose_level'] >= glucose_level_range[0]) & 
+                         (df['avg_glucose_level'] <= glucose_level_range[1])]
+
+fig_bar_glucose = px.bar(glucose_filtered_df, x='avg_glucose_level', y='stroke', 
+                         title='Strokes Over Glucose Level',
+                         labels={'avg_glucose_level': 'Average Glucose Level', 'stroke': 'Stroke'},
+                         range_x=[float(df['avg_glucose_level'].min()), float(df['avg_glucose_level'].max())],
+                         range_y=[0, 1],
+                         color='stroke',
+                         color_discrete_map={0: 'blue', 1: 'red'},
+                         text='stroke')
+st.plotly_chart(fig_bar_glucose)

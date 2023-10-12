e59de2dd234bb171cb7abf6b1da8d332f74e0390
abd3434 <145936250+abd3434@users.noreply.github.com>
2023-10-12 22:05:53 +0300

    Update nour.py

--- nour.py
+++ nour.py
@@ -8,66 +8,15 @@ df = pd.read_csv("stroke_data.csv")
 # Streamlit app title
 st.title("Stroke")
 
+# Definition of Stroke
+st.write("""
+A stroke occurs when the blood supply to part of your brain is interrupted or reduced, preventing brain tissue from getting oxygen and nutrients. Brain cells begin to die in minutes.
+""")
+
 # Title and paragraph about Stroke
 st.write("Stroke is a critical health condition that requires attention and understanding. It is a significant medical issue that impacts numerous individuals worldwide. Exploring and visualizing data related to stroke can provide insights into its prevalence, risk factors, and potential preventative measures.")
 
-# Filter the data based on conditions
-st.sidebar.header("Filter Data")
-
-# Gender Dropdown
-gender_options = ['All'] + df['gender'].unique().tolist()
-selected_gender = st.sidebar.selectbox("Select Gender", gender_options)
-
-# Hypertension Dropdown
-hypertension_options = ['All'] + df['hypertension'].unique().astype(str).tolist()
-selected_hypertension = st.sidebar.selectbox("Select Hypertension", hypertension_options)
-
-# Heart Disease Dropdown
-heart_disease_options = ['All'] + df['heart_disease'].unique().astype(str).tolist()
-selected_heart_disease = st.sidebar.selectbox("Select Heart Disease", heart_disease_options)
-
-# Married Dropdown
-ever_married_options = ['All'] + df['ever_married'].unique().tolist()
-selected_ever_married = st.sidebar.selectbox("Select Marital Status", ever_married_options)
-
-# Work Type Dropdown
-work_type_options = ['All'] + df['work_type'].unique().tolist()
-selected_work_type = st.sidebar.selectbox("Select Work Type", work_type_options)
-
-# Residence Type Dropdown
-residence_type_options = ['All'] + df['Residence_type'].unique().tolist()
-selected_residence_type = st.sidebar.selectbox("Select Residence Type", residence_type_options)
-
-# Smoking Status Dropdown
-smoking_status_options = ['All'] + df['smoking_status'].unique().tolist()
-selected_smoking_status = st.sidebar.selectbox("Select Smoking Status", smoking_status_options)
-
-# Age Slider
-age_range = st.sidebar.slider("Select Age Range", int(df['age'].min()), int(df['age'].max()), (int(df['age'].min()), int(df['age'].max())))
-
-# Average Glucose Level Slider
-avg_glucose_level_range = st.sidebar.slider("Select Average Glucose Level Range", float(df['avg_glucose_level'].min()), 
-                                            float(df['avg_glucose_level'].max()), 
-                                            (float(df['avg_glucose_level'].min()), float(df['avg_glucose_level'].max())))
-
-# BMI Slider
-bmi_range = st.sidebar.slider("Select BMI Range", float(df['bmi'].min()), float(df['bmi'].max()), 
-                              (float(df['bmi'].min()), float(df['bmi'].max())))
-
-# Apply filters
-filtered_df = df[
-    ((df['gender'] == selected_gender) | (selected_gender == 'All')) &
-    ((df['hypertension'].astype(str) == selected_hypertension) | (selected_hypertension == 'All')) &
-    ((df['heart_disease'].astype(str) == selected_heart_disease) | (selected_heart_disease == 'All')) &
-    ((df['ever_married'] == selected_ever_married) | (selected_ever_married == 'All')) &
-    ((df['work_type'] == selected_work_type) | (selected_work_type == 'All')) &
-    ((df['Residence_type'] == selected_residence_type) | (selected_residence_type == 'All')) &
-    ((df['smoking_status'] == selected_smoking_status) | (selected_smoking_status == 'All')) &
-    ((df['age'] >= age_range[0]) & (df['age'] <= age_range[1])) &
-    ((df['avg_glucose_level'] >= avg_glucose_level_range[0]) & (df['avg_glucose_level'] <= avg_glucose_level_range[1])) &
-    ((df['bmi'] >= bmi_range[0]) & (df['bmi'] <= bmi_range[1]))
-]
-
+# [Filtering Code Remains the Same as Your Original Code]
 
 # 3D Scatter Plot
 st.subheader("3D Scatter Plot of Age, Glucose Level, and BMI")
@@ -76,6 +25,10 @@ fig_3d = px.scatter_3d(filtered_df, x='age', y='avg_glucose_level', z='bmi', col
                         labels={'age': 'Age', 'avg_glucose_level': 'Average Glucose Level', 'bmi': 'BMI'},
                         color_discrete_map={0: 'blue', 1: 'red'})
 st.plotly_chart(fig_3d)
+st.write("""
+This 3D scatter plot visualizes the relationship between age, average glucose level, and BMI. 
+The distinction in colors indicates the presence (red) or absence (blue) of stroke. Observing any clusters or patterns can help identify potential risk factors.
+""")
 
 # Contour Plot
 st.subheader("Contour Plot of Age vs. Average Glucose Level")
@@ -84,11 +37,19 @@ fig_contour = px.density_contour(filtered_df, x='age', y='avg_glucose_level', co
                                  labels={'age': 'Age', 'avg_glucose_level': 'Average Glucose Level'},
                                  color_discrete_map={0: 'blue', 1: 'red'})
 st.plotly_chart(fig_contour)
+st.write("""
+The contour plot showcases the density of data points at various intersections of age and average glucose level. 
+Darker regions indicate a higher concentration of individuals, providing insight into common age and glucose levels.
+""")
 
 # Pie Chart
 st.subheader(f"Pie Chart: Distribution of Stroke for {selected_gender if selected_gender != 'All' else 'All Genders'}")
 fig_pie = px.pie(filtered_df, names='stroke', title=f"Distribution of Stroke for {selected_gender if selected_gender != 'All' else 'All Genders'}")
 st.plotly_chart(fig_pie)
+st.write("""
+This pie chart provides a clear representation of the proportion of individuals who have experienced a stroke based on the selected gender filter. 
+It helps in understanding the gender-specific prevalence of stroke.
+""")
 
 # Bar Chart with Range Slider
 st.subheader(f"Bar Chart: Average Glucose Level vs. BMI for Age {age_range[0]} - {age_range[1]}")
@@ -98,6 +59,10 @@ fig_bar = px.bar(filtered_df, x='avg_glucose_level', y='bmi', color='age',
                  range_x=[float(df['avg_glucose_level'].min()), float(df['avg_glucose_level'].max())],
                  range_y=[float(df['bmi'].min()), float(df['bmi'].max())])
 st.plotly_chart(fig_bar)
+st.write("""
+The bar chart visualizes the relationship between average glucose levels and BMI for individuals within the selected age range. 
+The color gradient, representing age, provides an added dimension to analyze how age might be influencing these parameters.
+""")
 
 # Sunburst Chart
 st.subheader("Sunburst Chart: Relationship between Smoking Status, Gender, and Stroke")
@@ -106,3 +71,7 @@ fig_sunburst = px.sunburst(filtered_df, path=['smoking_status', 'gender'], value
                            color_continuous_scale='Viridis',
                            labels={'smoking_status': 'Smoking Status', 'gender': 'Gender', 'stroke': 'Stroke'})
 st.plotly_chart(fig_sunburst)
+st.write("""
+The sunburst chart presents a hierarchical view of the relationship between smoking status, gender, and instances of stroke. 
+It allows for a multi-level exploration of how these factors might interplay in the prevalence of stroke.
+""")
